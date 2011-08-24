bb34de0351ea9f3df94537de84f4d1331f20ce04
James Hemming <jkhemming@jkhemming.co.uk>
2011-08-25 00:31:43 +0100

    Added a Brucie Bonus reminder when setting a prediction. Will later add number remaining. Removed some redundant comments in the CSS file.

--- connect.php
+++ connect.php
@@ -1,13 +1,13 @@
 <?php
 
 //connect to the database
-//$connection = mysql_connect("jkhemming.dns-systems.net", "jkhemming_prd", "ffzetecs16d");
-$connection = mysql_connect("localhost", "root", "");
+$connection = mysql_connect("jkhemming.dns-systems.net", "jkhemming_prd", "ffzetecs16d");
+//$connection = mysql_connect("localhost", "root", "");
 if (!$connection)
 {
 	header('Location: error.php?error=Failed+to+connect+to+database');
 }
 
-//mysql_select_db("jkhemming_prd", $connection);
-mysql_select_db("predikta", $connection);
+mysql_select_db("jkhemming_prd", $connection);
+//mysql_select_db("predikta", $connection);
 ?>
