f952066bf990af1398bf8238b90ac3e180b910eb
James Hemming <jkhemming@jkhemming.co.uk>
2014-06-12 00:27:36 +0100

    Forgot password and database name. Genius.

--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
 
 //connect to the database
-$connection = mysql_connect("jkhemming.dns-systems.net", "jkhemming_prd", "Ffzetecs16d");
+$connection = mysql_connect("jkhemming.dns-systems.net", "jkhemming_prd", "ffzetecs16d");
 //$connection = mysql_connect("localhost", "root", "");
 if (!$connection)
 {
 	header('Location: error.php?error=Failed+to+connect+to+database');
 }
 
-mysql_select_db("jkhemming_asaprd", $connection);
+mysql_select_db("jkhemming_prd", $connection);
 ?>
