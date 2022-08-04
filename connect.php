d508d509d15e7979fb54c038e9ebee78839ec8c6
James Hemming <james@jkhemming.uk>
2022-08-04 18:58:42 +0100

    Updated database connection.

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
 //connect to the database
-$connection = mysqli_connect("localhost:3306", "predikta", "k5zoM0@8", "predikta");
+$connection = mysqli_connect("localhost:3306", "predikta", "Uye4#063t", "predikta");
 
 if (!$connection)
 {
