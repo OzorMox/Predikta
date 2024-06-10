8f21ed0f837435226a3e531fff9de1f64568c571
James Hemming <james@jkhemming.uk>
2024-06-10 22:30:13 +0000

    Fixed reset.

--- processreset.php
+++ processreset.php
@@ -11,7 +11,7 @@ if ($_SESSION['admin'] == 1)
 	mysqli_query($connection, "DELETE FROM games");
 	mysqli_query($connection, "DELETE FROM results");
 	mysqli_query($connection, "DELETE FROM messages");
-	mysqli_query($connection, "UPDATE players SET brucies = 0, july = 0, august = 0, september = 0, october = 0, november = 0, december = 0, january = 0, february = 0, march = 0, april = 0, may = 0, june = 0, groupstage = 0, knockout = 0");
+	mysqli_query($connection, "UPDATE players SET brucies = 0, july = 0, august = 0, september = 0, october = 0, november = 0, december = 0, january = 0, february = 0, march = 0, april = 0, may = 0, june = 0, groupstage = 0, roundof16 = 0, quarters = 0, semis = 0, thefinal = 0");
 	mysqli_query($connection, "UPDATE players SET competition = '" . $_POST["competition"] . "' WHERE name = 'Admin'");
 	include("log.php");
 	$action = "Reset game, competition: " . $_POST["competition"];
