5c7ac60472f5934adfcac9a432789bad8dcf7b98
James Hemming <james@jkhemming.uk>
2026-06-01 21:11:49 +0000

    Set default blank value for avatar when adding a new player.

--- processaddplayer.php
+++ processaddplayer.php
@@ -22,7 +22,7 @@ if ($_SESSION['admin'] == 1)
 {
 	if ($_POST["player"] != "Admin" && $_POST["player"] != "Brucie")
 	{
-		mysqli_query($connection, "INSERT INTO players (name, password, admin, brucies) VALUES ('" . strip_tags($_POST["player"]) . "', 'default', 0, " . $_POST["brucies"] . ")");
+		mysqli_query($connection, "INSERT INTO players (name, password, avatar, admin, brucies) VALUES ('" . strip_tags($_POST["player"]) . "', 'default', '', 0, " . $_POST["brucies"] . ")");
 		include("log.php");
 		$action = "Added player: " . strip_tags($_POST["player"]) . ", " . $_POST["brucies"] . " Brucies";
 		writelog($action);
